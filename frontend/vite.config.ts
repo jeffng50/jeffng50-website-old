9300d07737a1066a8b504d7e251667936f9e6e8e
Jeff Ng <50jeffng@gmail.com>
2024-06-20 22:20:10 +0800

    fix: remove unnecessary polyfill

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import houdini from 'houdini/vite'
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-	plugins: [nodePolyfills(), houdini(), sveltekit()]
+	plugins: [houdini(), sveltekit()]
 });
