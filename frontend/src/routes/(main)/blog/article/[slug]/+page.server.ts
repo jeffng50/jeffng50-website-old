0887c5eccd70e9e69dbf15e5ef825d558f292d59
Jeff Ng <50jeffng@gmail.com>
2024-06-30 00:52:30 +0800

    fix: adding declare entries to try to fix mobile issues (slow)

---
+++ frontend/src/routes/(main)/blog/article/[slug]/+page.server.ts
@@ -0,0 +1,16 @@
+import type { EntryGenerator } from './$types';
+
+export const entries: EntryGenerator = () => {
+	return [
+		{ slug: 'asdadadasd' },
+		{ slug: 'asdasdsadas' },
+		{ slug: 'awdadwadawdwa34423' },
+		{ slug: 'awdadwadawdaw' },
+		{ slug: 'post' },
+		{ slug: 'review-and-opinion' },
+		{ slug: 'controversial-opinion-1' },
+		{ slug: 'test12313123' },
+		{ slug: 'movie-review-2' },
+		{ slug: 'movie-review' }
+	];
+};
